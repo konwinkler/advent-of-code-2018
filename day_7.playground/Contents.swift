29c8b6d1a75ee55d7b555e15c28ac85b9a367783
konwinkler <konrad.winkler@willowtreeapps.com>
2018-12-11 21:46:37 -0500

    day 7 part 1

--- day_7.playground/Contents.swift
+++ day_7.playground/Contents.swift
@@ -1,57 +1,99 @@
 import Foundation
 import PlaygroundSupport
 
-let fileUrl = playgroundSharedDataDirectory.appendingPathComponent("input_7_example.txt")
-var fileContents: String?
-do {
-    fileContents = try String(contentsOf: fileUrl)
-} catch {
-    print("Error reading contents: \(error)")
+func parseFile() -> [String.SubSequence]? {
+    let fileUrl = playgroundSharedDataDirectory.appendingPathComponent("input_7.txt")
+    var fileContents: String?
+    do {
+        fileContents = try String(contentsOf: fileUrl)
+    } catch {
+        print("Error reading contents: \(error)")
+    }
+    return fileContents?.split(separator: "\n")
 }
-let lines: [String.SubSequence]?  = fileContents?.split(separator: "\n")
 
 struct Graph {
-    var points: [Point]
-    var edges: [Edge]
+    var nodes: [Node]
+    
+    func availableNodes() -> [Node] {
+        return nodes.filter({ $0.precessors == []})
+    }
+    
+    func printNodes() -> () {
+        for node in nodes {
+            print("\(node) \(node.precessors) \(node.successors)")
+        }
+    }
+    
+    mutating func visit(_ node: Node) -> () {
+        node.visit()
+        self.nodes.removeAll(where: {$0 == node})
+    }
 }
 
-struct Point: Equatable, CustomStringConvertible {
+class Node: Equatable, Comparable, CustomStringConvertible {
+    static func < (lhs: Node, rhs: Node) -> Bool {
+        return lhs.description < rhs.description
+    }
+    
+    static func == (lhs: Node, rhs: Node) -> Bool {
+        return lhs.description == rhs.description
+    }
+    
     var description: String
+    var successors: [Node] = []
+    var precessors: [Node] = []
 
     init(_ description: String) {
         self.description = description
     }
+    
+    func visit() -> () {
+        for successor in successors {
+            successor.precessors.removeAll(where: {$0 == self})
+        }
+    }
+    
 }
 
-struct Edge: CustomStringConvertible {
-    var description: String
-    var source: Point
-    var target: Point
-    
-    init(_ source: Point, _ target: Point) {
-        self.source = source
-        self.target = target
-        self.description = "\(source)->\(target)"
+func main() -> () {
+    // parse graph from file
+    var graph = Graph(nodes: [])
+    let lines = parseFile()
+    for line in lines! {
+        let words = String(line).split(separator: " ")
+        var source = Node(String(words[1]))
+        var target = Node(String(words[7]))
+        
+        print("found rule \(source) to \(target)")
+        
+        // add points if they don't exist yet in the graph
+        if(!graph.nodes.contains(source)) {
+            graph.nodes.append(source)
+        } else {
+            let firstIndex = graph.nodes.firstIndex(of: source)
+            source = graph.nodes[firstIndex!]
+        }
+        if(!graph.nodes.contains(target)) {
+            graph.nodes.append(target)
+        } else {
+            let firstIndex = graph.nodes.firstIndex(of: target)
+            target = graph.nodes[firstIndex!]
+        }
+        
+        source.successors.append(target)
+        target.precessors.append(source)
     }
+
+    var orderOfNodes: String = ""
+    while(!graph.nodes.isEmpty) {
+        let availableNodes = graph.availableNodes().sorted()
+        let currentNode = availableNodes.first!
+        orderOfNodes = orderOfNodes + currentNode.description
+        graph.visit(currentNode)
+    }
+    print(orderOfNodes)
 }
 
-// parse graph from file
-var graph = Graph(points: [], edges: [])
-for line in lines! {
-    let words = String(line).split(separator: " ")
-    let source = Point(String(words[1]))
-    let target = Point(String(words[7]))
-    
-    print("found rule \(source) to \(target)")
-    
-    // add points if they don't exist yet in the graph
-    if(!graph.points.contains(source)) {
-        graph.points.append(source)
-    }
-    if(!graph.points.contains(target)) {
-        graph.points.append(target)
-    }
-    
-    graph.edges.append(Edge(source, target))
-}
-print(graph)
+main()
+
